fedf9764e5ab6471dad1d956e3e27387f54b89fd
Maciej <walder_m@wp.pl>
2022-11-23 17:52:17 +0100

    Zadanie domowe aplikacja Ochroniarz

--- src/Ochroniarz.java
+++ src/Ochroniarz.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Ochroniarz {
 
     public static void main(String[] args) {
+        System.out.println("Witaj w Aplikacji Ochroniarz");
         Scanner scanner=new Scanner(System.in);
         System.out.println("Poniżej wpisz swoje imię:");
         String firstName=scanner.nextLine();
@@ -13,16 +14,13 @@ public class Ochroniarz {
         System.out.println("Poniżej wpisz swój wiek:");
         int age=scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Poniżej wpisz czy jesteś w związku Yes/No:");
-        String relationship= scanner.nextLine();
-        Boolean yes=true;
-        Boolean no=false;
-        if (yes) {
-            System.out.println(yes);
+        System.out.println("Poniżej wpisz czy jesteś w związku True/Fals:");
+        boolean relationship= scanner.nextBoolean();
+        if (relationship==true) {
+            System.out.println("Witam Cię " +firstName+ " " + lastName + " z " +address + " masz " + age + " lat i " + " jesteś w związaku zatem WITAM w KLUBIE TESTERSI :)");
         }else {
-            System.out.println(no);
+
         }
-        System.out.println("Witam Cię " +firstName+ " " + lastName + " w KLUBIE TESTERSI :)");
 
     }
 
